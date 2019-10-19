5f776000dc8b06acc3a31a6a33b9a9ce22876fb3
sprite <tech.01170917@gmail.com>
2019-10-19 19:21:33 +0900

    create base

--- app/src/main/java/com/example/janken/MainActivity.java
+++ app/src/main/java/com/example/janken/MainActivity.java
@@ -8,6 +8,10 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +19,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView text = (TextView)findViewById(R.id.text);
+        final TextView subtext = (TextView)findViewById(R.id.subtext);
+        Button rock = (Button)findViewById(R.id.rock);
+        Button scissors = (Button)findViewById(R.id.scissors);
+        Button paper = (Button)findViewById(R.id.paper);
+
+        //opponentHand 1 "rock"
+        //opponentHand 2 "scissors"
+        //opponentHand 3 "paper"
+        rock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int opponentHand = decideOpponentHand();
+                String opponentHandText = changeTextOpponentHand(opponentHand);
+                decideGame(1, opponentHand, subtext);
+                text.setText(opponentHandText);
+            }
+        });
+        scissors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int opponentHand = decideOpponentHand();
+                String opponentHandText = changeTextOpponentHand(opponentHand);
+                decideGame(2, opponentHand, subtext);
+                text.setText(opponentHandText);
+            }
+        });
+        paper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int opponentHand = decideOpponentHand();
+                String opponentHandText = changeTextOpponentHand(opponentHand);
+                decideGame(3, opponentHand, subtext);
+                text.setText(opponentHandText);
+            }
+        });
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    }
+
+    String changeTextOpponentHand(int hand) {
+        String handText = "";
+        if (hand == 1) handText = "ぐー";
+        else if (hand == 2) handText = "ちょき";
+        else if (hand == 3) handText = "ぱー";
+        return handText;
+    }
+
+    int decideOpponentHand() {
+        Random rnd = new Random();
+        int hand = rnd.nextInt(3)+1;
+        return hand;
+    }
+
+    void decideGame (int playerHand, int opponentHand, TextView decidetext) {
+        String decision;
+        if (playerHand == opponentHand) decision = "あいこ";
+        else if((playerHand == 3 && opponentHand == 1) || (playerHand+1 == opponentHand)) decision = "勝ち";
+        else decision = "負け";
+        decidetext.setText(decision);
     }
 
     @Override
